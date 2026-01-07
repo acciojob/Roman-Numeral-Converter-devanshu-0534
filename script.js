908a99057d4f01c2ff41f55c408ae28868f9f736
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-07 19:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 function convertToRoman(num) {
-	  if (num === 0) return "";
+  if (num === 0) return { roman: "" };
 
-    let result = "";
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-const keys = Object.keys(obj).map(Number);
+  let result = "";
 
-  for (let i = 0; i < keys.length; i++) {
-    const [symbol, valueыг] = obj[i];
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
+
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    const symbol = obj[i][0];
     const value = obj[i][1];
-    const sym = obj[i][0];
 
-    // Normal repetition
-    while (num >= value) {
-      result += sym;
-      num -= value;
+    // ✅ handle subtractive case FIRST (IV, IX, XL, XC, CD, CM)
+    if (i + 2 < Object.keys(obj).length) {
+      const subSymbol = obj[i + 2][0];
+      const subValue = value - obj[i + 2][1];
+
+      if (num >= subValue) {
+        result += subSymbol + symbol;
+        num -= subValue;
+        continue;
+      }
     }
 
-    // Subtractive case (look 2 steps ahead)
-    if (i + 2 < keys.length) {
-      const subValue = value - obj[i + 2][1];
-      if (num >= subValue) {
-        result += obj[i + 2][0] + sym;
-        num -= subValue;
-      }
+    // normal repetition
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
   }
 
-  return result;
-
+  return { roman: result };
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
