b0574da851d072d984587972116689289e67e036
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-07 11:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function convertToRoman(num) {
+	if (num <= 0 || num > 3999) return "";
   	const obj = {
       0:['M',1000], 
       1:['D', 500], 
@@ -8,22 +9,24 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	let result = "";
+  const values = [
+    1000, 900, 500, 400,
+    100, 90, 50, 40,
+    10, 9, 5, 4, 1
+  ];
 
-   const symbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-   const values  = [1000, 500, 100, 50, 10, 5, 1];
+  const symbols = [
+    "M", "CM", "D", "CD",
+    "C", "XC", "L", "XL",
+    "X", "IX", "V", "IV", "I"
+  ];
 
   // Loop through all values
   for (let i = 0; i < values.length; i++) {
     while (num >= values[i]) {
       result += symbols[i];
       num -= values[i];
-    }
-    if (i < values.length - 2) {
-      const subValue = values[i] - values[i + 2];
-      if (num >= subValue) {
-        result += symbols[i + 2] + symbols[i];
-        num -= subValue;
-      }
     }
   }
 
